1971004232a5467b4f4491f620f13f443a7e229f
SoundSlab <administrator@Mac-mini.local>
2020-11-04 14:12:03 -0500

    commit

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/petful',
   API_BASE_URL:
     process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api",
 };
